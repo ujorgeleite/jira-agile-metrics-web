804e3d8469240d2321efa12f9ece53ebd24e584e
ujorgeleite <jorgefernandodamasio@gmail.com>
2021-05-06 17:38:09 -0300

    add(generation): add generation button and delete

--- src/components/InputFiles/index.tsx
+++ src/components/InputFiles/index.tsx
@@ -7,15 +7,26 @@ import styles from "./style.module.scss"
 
 export function InputFiles() {
   const [files, setFiles] = useState([String])
+  const [refresh, setRefresh] = useState(false)
 
   const getFiles = async () => {
     const { data } = await api.get('List');
     setFiles(data)
   }
 
+  const generateFiles = async () => {
+    await api.post('/Process')
+    setRefresh(!refresh)
+  }
+
+  const removeFiles = async () => {
+    await api.delete('/Delete')
+    setRefresh(!refresh)
+  }
+
   useEffect(() => {
     getFiles()
-  }, [])
+  }, [refresh])
 
   return (
     <div className={styles.InputList}>
@@ -24,12 +35,16 @@ export function InputFiles() {
         {
           files.map((file, index) => {
             return (<li key={index}>
-              <FaFileExcel className={styles.icon}/>
+              <FaFileExcel className={styles.icon} />
               {file.name}
             </li>)
           })
         }
       </ul>
+      <div className={styles.buttons}>
+        <button className={styles.generate} type="button" onClick={generateFiles}>Gerar Arquivos</button>
+        <button className={styles.remove} type="button" onClick={removeFiles}>Remover Arquivos</button>
+      </div>
     </div>
   )
 }
