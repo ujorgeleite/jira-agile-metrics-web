045fa0d9a32032006f2c05f1e84afe33c0353905
Roger <contatorooger@gmail.com>
2021-05-22 17:41:44 -0300

    change components style

--- src/components/DownloadFiles/index.tsx
+++ src/components/DownloadFiles/index.tsx
@@ -30,14 +30,16 @@ export function DownloadFiles() {
   }
 
   const showFiles = (files) => {
-    if(!files || files.length ==0 ){
+    if (!files || files.length == 0) {
       return <p className={styles.DownloadList__alert}>Nenhum arquivo</p>
-    }else{
+    } else {
       return files.map((file, index) => {
         return (
           <li key={index}>
             <button onClick={downloadFile} name={file.name}>
-              <FaFileExcel className={styles.icon} />
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
+                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z" />
+              </svg>
               {file.name}
             </button>
           </li>)
@@ -52,8 +54,8 @@ export function DownloadFiles() {
   return (
     <div className={styles.DownloadList}>
       <span className={styles.DownloadList__titulo}>Arquivos para Download</span>
-      <ul>
-     
+      <ul >
+
         {
           showFiles(files)
         }
